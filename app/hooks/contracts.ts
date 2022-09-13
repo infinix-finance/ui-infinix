99574c287b7e12fa2b8a9b3b5c1cec5f2b0f3f62
Juraj Bačovčin <hehe100596@yahoo.com>
2022-09-13 13:26:14 +0200

    fix: handling approvals in case of existing allowance and increasing gas limit

--- app/hooks/contracts.ts
+++ app/hooks/contracts.ts
@@ -1,6 +1,6 @@
 import create from "zustand";
 import { useEffect } from "react";
-import { Contract, providers, utils } from "ethers";
+import { Contract, BigNumber, providers, utils } from "ethers";
 
 import { useStore } from "@/stores/root";
 import contractConfig from "@/defi/contracts";
@@ -13,6 +13,17 @@ interface ContractList {
 interface ContractStore extends ContractList {
   setContracts: (contracts: ContractList) => void;
 }
+
+// TODO: We should decide whether we want to use this or not
+// we are using this to speed up transactions for testing
+const gasAmount = (chainId: number | undefined) => {
+  switch (chainId) {
+    case 43113:
+      return { gasLimit: 1000000 };
+    default:
+      return {};
+  }
+};
 
 const useContractStore = create<ContractStore>((set) => ({
   setContracts: (contracts: ContractList) =>
@@ -41,8 +52,9 @@ export const useContractConnection = () => {
 };
 
 export const useClearingHouse = () => {
-  const { active } = useStore((state) => state.connection);
+  const { active, account, chainId } = useStore((state) => state.connection);
   const { signer, clearingHouse } = useContractStore((state) => state);
+  const gasLimit = gasAmount(chainId);
 
   const openPosition = async (
     amm: string,
@@ -58,20 +70,37 @@ export const useClearingHouse = () => {
       const amountToSpend = utils.parseEther(quoteAssetAmount);
       const erc20 = new Contract(quoteAsset, contractConfig.erc20.abi, signer);
 
-      // approve spending first
-      const approval = await erc20.approve(
-        contractConfig.clearingHouse.addr,
-        amountToSpend
+      const allowance: BigNumber = await erc20.allowance(
+        account,
+        contractConfig.clearingHouse.addr
       );
-      await approval.wait();
 
-      // then open position
+      // approve spending if necessary first
+      if (allowance.lt(amountToSpend)) {
+        // to increase allowance we first have to reduce it to 0
+        if (allowance.gt(0)) {
+          const reduction = await erc20.approve(
+            contractConfig.clearingHouse.addr,
+            0,
+            gasLimit
+          );
+          await reduction.wait();
+        }
+        const approval = await erc20.approve(
+          contractConfig.clearingHouse.addr,
+          amountToSpend,
+          gasLimit
+        );
+        await approval.wait();
+      }
+
       const result = await clearingHouse.openPosition(
         amm,
         side,
         [amountToSpend],
         [utils.parseEther(leverage)],
-        [utils.parseEther(baseAssetAmountLimit)]
+        [utils.parseEther(baseAssetAmountLimit)],
+        gasLimit
       );
       await result.wait();
     } catch (error) {
@@ -83,9 +112,11 @@ export const useClearingHouse = () => {
     if (!active || !clearingHouse) return;
 
     try {
-      const result = await clearingHouse.closePosition(amm, [
-        utils.parseEther(quoteAssetAmountLimit),
-      ]);
+      const result = await clearingHouse.closePosition(
+        amm,
+        [utils.parseEther(quoteAssetAmountLimit)],
+        gasLimit
+      );
       await result.wait();
     } catch (error) {
       console.error(error);
