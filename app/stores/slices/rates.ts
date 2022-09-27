30cac3360d5288f2e9274987ae86a56f1a1b77a1
Daniel Palanki <danielp@advancedblockchain.com>
2022-09-27 10:26:05 +0200

    test commit

--- app/stores/slices/rates.ts
+++ app/stores/slices/rates.ts
@@ -1,24 +1,13 @@
-import BigNumber from "bignumber.js";
-
 import { MarketId, PairId } from "@/defi";
 import { AppState, CustomStateCreator } from "../types";
 
 interface RatesProps {
   market: MarketId;
   pair: PairId;
-  exchangeRate: BigNumber | number;
-  percentageChange: number;
-  percentageValue: string;
-  markPrice: string;
-  indexPrice: string;
-  funding: string;
-  volumeValue: string;
-  startMillis: number;
 }
 
 export interface RatesSlice {
   rates: RatesProps & {
-    fetchDetails: () => void;
     changeMarket: (marketId: MarketId) => void;
     changePair: (pairId: PairId) => void;
   };
@@ -28,32 +17,13 @@ export const createRatesSlice: CustomStateCreator<RatesSlice> = (set, get) => ({
   rates: {
     market: MarketId.Crypto,
     pair: PairId.BTCUSDC,
-    exchangeRate: new BigNumber(0),
-    percentageChange: 0,
-    percentageValue: "$0.00",
-    markPrice: "$0.00",
-    indexPrice: "$0.00",
-    funding: "0.00%",
-    volumeValue: "$0.00",
-    startMillis: 0,
-
-    fetchDetails: () =>
-      set(function fetchDetails(state: AppState) {
-        state.rates = {
-          ...state.rates,
-          percentageChange: 21.3456,
-          percentageValue: "$210.00",
-          markPrice: "$1.50",
-          indexPrice: "$2.40",
-          funding: "0.0121%",
-          volumeValue: "$234.56",
-          startMillis: 2000000,
-        };
-      }),
 
     changeMarket: (marketId: MarketId) =>
       set(function changeMarket(state: AppState) {
         state.rates.market = marketId;
+
+        This comment since it's not preceeded by 2 forward slashes, will make 
+        this code useless or probably even the whole application would stop working.
       }),
 
     changePair: (pairId: PairId) =>
@@ -62,6 +32,3 @@ export const createRatesSlice: CustomStateCreator<RatesSlice> = (set, get) => ({
       }),
   },
 });
-
-export const getExchangeRate = (state: AppState): BigNumber =>
-  state.rates.exchangeRate as BigNumber;
